19a4de14d26b1d322409e4df3d2bf70bd4e9e1fd
laerling <laerling@posteo.de>
2019-04-18 12:35:29 +0200

    Implement more tests
    - parse_erroneous_entry
    - new_boxes_from_files
    - save_boxes
    - move_entry

--- src/boxes/tests.rs
+++ src/boxes/tests.rs
@@ -1,6 +1,6 @@
-use boxes::{Boxes, BOX_DEFAULT_CAPACITY, Entry};
+use boxes::{Boxes, BOX_DEFAULT_CAPACITY, Entry, SelectedEntry};
 use std::fs::File;
-use std::io::Write;
+use std::io::{Result, Write, BufRead, BufReader};
 
 #[test]
 fn new_boxes_statelessnes() {
@@ -21,18 +21,29 @@ fn new_boxes_default_capacity() {
 
 #[test]
 fn parse_entry() {
-    panic!("TODO");
+    let e: Entry = "foo = bar".parse().expect("Couldn't parse string into Entry");
+    assert_eq!(e.lhs, "foo");
+    assert_eq!(e.rhs, "bar");
+}
+
+#[test]
+fn parse_erroneous_entry() {
+    "erroneous entry".parse::<Entry>().expect_err("Erroneous entry was passed without expected error");
 }
 
 #[test]
 fn new_boxes_from_files() {
-    // We can't test for erroneous entries, cause they cause Boxes::from_files to panic.
-    // They're tested in tests::parse_entry.
+
+    // create files
     for i in 0..5 {
         let mut box_file = File::create(format!("/tmp/box{}", i+1).as_str()).expect("Can't create box files for test");
         box_file.write(b"foo = bar").expect("Can't write to box files for test");
     }
+
+    // create boxes
     let b = Boxes::from_files(&String::from("/tmp")).expect("Can't create boxes from files");
+
+    // check boxes
     for i in 0..5 {
         assert_eq!(b.boxes[i].len(), 1);
         assert_eq!(b.boxes[i][0], Entry { lhs: String::from("foo"), rhs: String::from("bar") });
@@ -41,7 +52,28 @@ fn new_boxes_from_files() {
 
 #[test]
 fn save_boxes() {
-    panic!("TODO");
+
+    // create boxes
+    let mut b = Boxes::new();
+    for i in 0..5 {
+        // write multiple entries in order to test if multiple lines are written correctly
+        for _ in 0..i+1 {
+            b.boxes[i].push(Entry { lhs: String::from("foo"), rhs: String::from("bar") });
+        }
+    }
+
+    // save
+    b.save(&String::from("/tmp"));
+
+    // check files
+    for i in 0..5 {
+        let box_file = File::open(format!("/tmp/box{}", i+1).as_str()).expect("Can't open box file");
+        let lines: Vec<String> = BufReader::new(box_file).lines().collect::<Result<_>>().expect("Cannot read lines from box file");
+        assert_eq!(lines.len(), i+1);
+        for line in lines {
+            assert_eq!(line, "foo = bar");
+        }
+    }
 }
 
 #[test]
@@ -52,6 +84,41 @@ fn select_random_entry_from_boxes() {
 
 #[test]
 fn move_entry() {
-    // TODO check moving of both successful and unsuccessful entry
-    panic!("TODO");
+
+    // create entries
+    let e1 = Entry { lhs: String::from("e1 lhs"), rhs: String::from("e1 lhs") };
+    let e2 = Entry { lhs: String::from("e2 lhs"), rhs: String::from("e2 lhs") };
+
+    // create boxes
+    let mut b = Boxes::new();
+    b.boxes[0].push(e1.clone());
+    b.boxes[4].push(e2.clone());
+
+    // check left box boundary
+    let e = e1.clone();
+    b.move_entry(SelectedEntry { lhs: e.lhs, rhs: e.rhs, box_i: 0, entry_i: 0 }, false);
+    assert_eq!(b.boxes[0].len(), 1); // entry still in 0...
+    assert_eq!(b.boxes[1].len(), 0); // and not moved to 1
+    assert_eq!(b.boxes[0][0], e1);
+
+    // check right box boundary
+    let e = e2.clone();
+    b.move_entry(SelectedEntry { lhs: e.lhs, rhs: e.rhs, box_i: 4, entry_i: 0 }, true);
+    assert_eq!(b.boxes[4].len(), 1); // entry still in 4...
+    assert_eq!(b.boxes[3].len(), 0); // and not moved to 3
+    assert_eq!(b.boxes[4][0], e2);
+
+    // check moving forward
+    let e = e1.clone();
+    b.move_entry(SelectedEntry { lhs: e.lhs, rhs: e.rhs, box_i: 0, entry_i: 0 }, true);
+    assert_eq!(b.boxes[0].len(), 0); // element moved from 0...
+    assert_eq!(b.boxes[1].len(), 1); // to 1
+    assert_eq!(b.boxes[1][0], e1);
+
+    // check moving backward
+    let e = e2.clone();
+    b.move_entry(SelectedEntry { lhs: e.lhs, rhs: e.rhs, box_i: 4, entry_i: 0 }, false);
+    assert_eq!(b.boxes[4].len(), 0); // element moved from 4...
+    assert_eq!(b.boxes[3].len(), 1); // to 3
+    assert_eq!(b.boxes[3][0], e2);
 }
